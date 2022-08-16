c62b122bd1505ef65f98069dfada12d0d6317f7c
Minsuk Daniel Kim <danielhankim4418@gmail.com>
2022-08-16 14:54:52 -0400

    Update nw_data_io.c

--- c_mylib/nw_data_io.c
+++ c_mylib/nw_data_io.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 // file handling -> python
-// 1. match file format (i:j -> i;j, i j -> i;j, etc.) 
+// 1. match file format (i:j -> i;j, i j -> i;j, etc.)
 // 2. check starting index & adjust starting to 0
 // 3. check missing nodes between node_0 and node_N-1
-// 4. if nodes are missed, give good index 
+// 4. if nodes are missed, give good index
 
 int get_nw_size(char *fn) {
     FILE *fp;
@@ -35,7 +35,7 @@ int get_nw_size(char *fn) {
 int get_adj_list(char *fn, int **adj, int *deg, int N) {
     FILE *fp;
     int i, j;
-    
+
     fp = fopen(fn, "r");
     if (fp == NULL) {
         printf("!!! FAIL TO OPEN YOUR FILE !!! \n");
@@ -52,4 +52,25 @@ int get_adj_list(char *fn, int **adj, int *deg, int N) {
     }
 
     return 0;
+}
+
+
+void save_nw_data(char *fn, int **adj, int *deg, int N) {
+    FILE *fp;
+    int i, j;
+
+    fp = fopen(fn, "w");
+
+    if (fp == NULL) {
+        printf("!!! FAIL TO MAKE YOUR FILE !!! \n");
+        printf("!!! CHECK YOUR FILE AGAIN  !!! \n");
+        return -1;
+    } else {
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < deg[i]; j++) {
+                fprintf(fp, "%d;%d\n", i, adj[i][j]);
+            }
+        }
+    }
+    fclose(fp);
 }
